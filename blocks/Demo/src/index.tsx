304cceb832969238cd4d14304b1ebf39479dc9e3
yangshuai <704807396@qq.com>
2019-12-24 15:31:39 +0800

    添加布局测试模块

--- blocks/Demo/src/index.tsx
+++ blocks/Demo/src/index.tsx
@@ -1,17 +1,19 @@
-import React, { FunctionComponent, useEffect } from 'react';
+import React, { FunctionComponent } from 'react';
 
 interface IProps {
 }
 
 const Component: FunctionComponent<IProps> = props => {
-  useEffect(() => {
-
-  }, []);
-
   return (
-    <>
-      INSERT_BLOCK_PLACEHOLDER:DEMO
-    </>
+    <div>
+      <header>INSERT_BLOCK_PLACEHOLDER:HEADER</header>
+      <div>
+        <main>INSERT_BLOCK_PLACEHOLDER:CONTENT</main>
+        <nav>INSERT_BLOCK_PLACEHOLDER:NAV</nav>
+        <aside>INSERT_BLOCK_PLACEHOLDER:ASIDE</aside>
+      </div>
+      <footer>INSERT_BLOCK_PLACEHOLDER:FOOTER</footer>
+    </div>
   )
 };
 
