f1789caf73c7d958f99588ccccbed21c405c3a7e
yangshuai <704807396@qq.com>
2020-01-08 11:12:13 +0800

    增加栅格布局

--- layouts/grid-basic-3/src/index.tsx
+++ layouts/grid-basic-3/src/index.tsx
@@ -4,7 +4,13 @@ import { Col, Row } from 'antd';
 export default function() {
   return (
     <Row>
-      <Col span={24}>
+      <Col span={8}>
+        INSERT_BLOCK_PLACEHOLDER:Col
+      </Col>
+      <Col span={8}>
+        INSERT_BLOCK_PLACEHOLDER:Col
+      </Col>
+      <Col span={8}>
         INSERT_BLOCK_PLACEHOLDER:Col
       </Col>
     </Row>
