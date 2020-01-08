594719e7f260906507dde9d53d79b996f8fae739
yangshuai <704807396@qq.com>
2020-01-08 10:56:11 +0800

    增加测试栅格布局

---
+++ layouts/grid-basic-1/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Col, Row } from 'antd';
+
+export default function() {
+  return (
+    <Row>
+      <Col span={24}>
+        INSERT_BLOCK_PLACEHOLDER:Col
+      </Col>
+    </Row>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import EmptyPage from '../blocks/EmptyPage/src';
-import ProfileBasic from '../blocks/ProfileBasic/src';
-import FormBasicForm from '../blocks/FormBasicForm/src';
 
 const Block: React.FC = () => {
   return (
-    <FormBasicForm/>
+    <div>
+    </div>
   );
 };
 
