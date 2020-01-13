6944dd6a74a2ecd392a44eddf38a9f93adb7257a
yangshuai <704807396@qq.com>
2020-01-13 09:49:24 +0800

    1.增加路由预览 2.更新插件和MD

--- README.md
+++ README.md
@@ -9,39 +9,31 @@ yarn create umi --type=block
 yarn start
 ```
 
-### 三、新建区块
+### 三、新建模块、区块、布局
 ```
-1.前往项目 blocks 目录下 -> 以区块名称建立目录
+1.分别前往项目 templates、blocks、layouts 目录下 -> 以名称建立目录
 
-2.在新建区块目录下，分别建立：
-1).src目录（区块代码目录）
-2).package.json（区块描述信息）
+2.在相应名称目录下，分别建立：
+1).src目录（代码目录，必须有一个index.tsx文件）
+2).package.json（描述信息）
 3).snapshot.png（预览图片）
 
-3.在上面步骤创建的 src 目录下，编写区块代码
-
-4.在项目 src -> index.tsx 中引用新建的区块文件，启动并预览效果
+3.编写代码
 ```
 
 ### 四、发布区块
 ```
-1.在项目 umi-block.json 中，按照格式添加相应的区块描述
-
-2.将代码合并到 master 分支，并推送即可
+1.将代码合并到 master 分支，并推送即可
 ```
 
-### 预览区块
+### 预览区块（LAYOUT = ant-design-pro-user | ant-design-pro | blankLayout）
 ```
-1.预览区块
-cross-env BLOCK=blocks/demo yarn start
+1.预览区块（暂时不用）
+cross-env BLOCK=blocks/demo LAYOUT=ant-design-pro yarn start
 
-2.区块 layout
-1).cross-env BLOCK=blocks/demo LAYOUT=blankLayout yarn start
-
-// 报错，不能使用
-2).cross-env BLOCK=blocks/demo LAYOUT=ant-design-pro-user yarn start
-
-3).cross-env BLOCK=blocks/demo LAYOUT=ant-design-pro yarn start
+2.路由预览
+例如：
+/templates/EmptyPage
 ```
 
 
--- plugin.js
+++ plugin.js
@@ -1,9 +1,16 @@
 import { join } from 'path';
 import { readdirSync } from 'fs';
 
-if (process.env.BLOCK) {
-  process.env.PAGES_PATH = join(process.env.BLOCK, 'src');
-}
+// 暂时不用
+// if (process.env.BLOCK) {
+//   process.env.PAGES_PATH = join(process.env.BLOCK, 'src');
+// }
+// function getRoute() {
+//   return {
+//     path: '/',
+//     component: join(process.env.BLOCK, 'src', 'index.tsx'),
+//   };
+// }
 
 function getRoutes(type) {
   const dirs = readdirSync(join(__dirname, type));
@@ -18,13 +25,6 @@ function getRoutes(type) {
   return routes;
 }
 
-function getRoute() {
-  return {
-    path: '/',
-    component: join(process.env.BLOCK, 'src', 'index.tsx'),
-  };
-}
-
 export default (api) => {
   api.modifyRoutes(routes => {
     routes[0].routes = [
@@ -32,7 +32,7 @@ export default (api) => {
       ...getRoutes('blocks'),
       ...getRoutes('templates'),
       ...getRoutes('layouts'),
-      ...routes[0].routes.slice(1),
+      // ...routes[0].routes.slice(1),
     ];
     return routes;
   });
